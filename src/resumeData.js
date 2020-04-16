e61204512d0356f1b3b2cba9a2f4af3587d7a062
= <thenadjib@gmail.com>
2020-04-16 16:11:02 -0700

    first and last commit

---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": " Nadjib Niyonkuru",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Mrnadjiboss/coding_12_les_chefs",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/mrnadjib",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":" I am a self taught Full Stack Web Developer from Mlengeek, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Mrnadjiboss",
+    "education":[
+      {
+        "UniversityName":"catholic university of louvain-la-neuve,",
+        "specialization":"business engeneering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Personal"
+      },
+      {
+        "UniversityName":"Molengeek",
+        "specialization":"Formation intensive de 6mois",
+        "MonthOfPassing":"Décembre 2019",
+        "YearOfPassing":"Juin 2020",
+        "Achievements":"Full Stack Developer"
+      }
+    ],
+    "work":[
+      {
+        // "CompanyName":"Some Company",
+        // "specialization":"Some specialization",
+        // "MonthOfLeaving":"Jan",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"my skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+        
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }, 
+      {
+        "skillname":"scss"
+      },
+      {  "skillname":"Sass"
+      },
+      {  "skillname":"Python"
+    }
+      
+    ],
+    // "portfolio":[
+    //   {
+    //     "name":"project1",
+    //     "description":"emporium",
+    //     // "imgurl":"images/portfolio/phone.jpg"
+    //   },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    // ],
+    "testimonials":[
+      {
+        "description":"I am very motivated and flexible",
+        "name":"Niyonkuru Nadjib"
+      },
+      {
+        "description":"i am gifted with great intuition",
+        "name":"Nadjib Niyonkuru"
+      }
+    ]
+  }
+  
+  export default resumeData
